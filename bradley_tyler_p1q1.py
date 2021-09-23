4b9ddf3215832ca18c4658354c46b5b057142b98
Tyler Bradley <tylergbradley05@gmail.com>
2021-09-23 11:31:10 -0700

    jj

---
+++ bradley_tyler_p1q1.py
@@ -0,0 +1,28 @@
+# Quiz Creation Activity
+
+# Quiz has 5 questions the user will answer
+# It will keep track of score and give
+# a final result
+
+print("What is 2 + 5?")
+q1_ans = float(input())
+if q1_ans == 7:
+    print("Great job!")
+else:
+    print("Better luck next time.")
+
+print("What is Obama's last name?")
+q2_ans = input()
+if q2_ans == "Obama" or "obama":
+    print("Wow, you're smart!")
+else:
+    print("Wrong.")
+
+print("How many stars are on the U.S. flag?")
+q3_ans = float(input())
+if q3_ans == 50:
+    print("You're very smart, Great Job!")
+
+print()
+q4_ans == input()
+
