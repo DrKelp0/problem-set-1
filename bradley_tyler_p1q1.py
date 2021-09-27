989188d6bc24614ec63e9e0b3f67e3baf10df18a
Tyler Bradley <tylergbradley05@gmail.com>
2021-09-27 12:46:21 -0700

    fixed number errors and score counting

--- bradley_tyler_p1q1.py
+++ bradley_tyler_p1q1.py
@@ -10,33 +10,33 @@ print("What is 2 + 5?")
 q1_ans = float(input())
 if q1_ans == 7:
     print("Great job!")
-    score_end + 1
+    score_end = score_end + 1
 else:
     print("Better luck next time.")
-    score_end = + 0
+    score_end = score_end + 0
 print(f"You have {score_end} questions correct so far.")
 
 print("What is Obama's last name?")
 q2_ans = input()
 if q2_ans == "Obama":
     print("Wow, you're smart!")
-    score_end + 1
+    score_end = score_end + 1
 elif q2_ans == "obama":
     print("Wow, you're smart!")
-    score_end + 1
+    score_end = score_end + 1
 else:
     print("Wrong.")
-    score_end + 0
+    score_end = score_end + 0
 print(f"You have {score_end} questions correct so far.")
 
 print("How many stars are on the U.S. flag?")
 q3_ans = float(input())
 if q3_ans == 50:
     print("You're pretty smart, Great Job!")
-    score_end + 1
+    score_end = score_end + 1
 else:
     print("You are correctn't")
-    score_end + 0
+    score_end = score_end + 0
 print(f"You have {score_end} questions correct so far.")
 
 print("Answer this math question")
@@ -44,32 +44,33 @@ print("3 + (6(11 + 1 - 4)) / 8 * 2")
 q4_ans = float(input())
 if q4_ans == 15:
     print("Amazing work, but can you get the last one right?")
-    score_end + 1
+    score_end = score_end + 1
 else:
     print("Incorrect")
-    score_end + 0
+    score_end = score_end + 0
 print(f"You have {score_end} questions correct so far.")
 
 print("How do you spell pneumonoultramicroscopicsilicovolcanoconiosis")
 q5_ans = input()
 if q5_ans == "pneumonoultramicroscopicsilicovolcanoconiosis" :
     print("You are a genius!")
-    score_end = + 1
+    score_end = score_end + 1
 else:
     print("Nice try")
+    score_end = score_end + 0
 
 print(f"You got {score_end} questions right in total")
-if score_end = 5
+if score_end == 5:
     print("You are part of the 1% smartest people in the world!")
-elif score_end = 4
+elif score_end == 4:
     print("You are clearly very smart, but you could be smarter.")
-elif score_end = 3
+elif score_end == 3:
     print("You have some knowledge, but lack a meaningful amount.")
-elif score_end = 2
+elif score_end == 2:
     print("You still have a ways to go.")
-elif score_end = 1
+elif score_end == 1:
     print("You need lots of work.")
-elif score_end = 0
+elif score_end == 0:
     print("You are very dumb")
 
 
