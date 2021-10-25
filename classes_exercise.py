df72b1e734c1511462afd4f3113f843203717eeb
Tyler Bradley <tylergbradley05@gmail.com>
2021-10-25 12:50:11 -0700

    New function - increased darkness of image

---
+++ classes_exercise.py
@@ -0,0 +1,34 @@
+
+
+import math
+
+
+
+class Vehicle:
+    """Thing used for transport
+
+    Attributes:
+        name: str
+        max_speed: int
+        capacity: int
+    """
+
+    def __init__(self):
+        self.name = Tonymobile
+        self.max_speed = 300
+        self.capacity = 100
+
+    for max_speed in range(300):
+        print("vroom")
+
+class Bus(Vehicle):
+    """A vehicle that is very large and carries
+      passengers
+
+    Attributes:
+        fare: Cost of riding the bus
+    """
+
+    def __init__(self, age: float) -> None
+
+
