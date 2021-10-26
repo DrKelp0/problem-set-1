1ff87219ea9352bfe42c201aa409200d5cf703ad
Tyler Bradley <tylergbradley05@gmail.com>
2021-10-26 09:22:27 -0700

    First commit - uses the method instead of "if" statements

--- classes_exercise.py
+++ classes_exercise.py
@@ -1,6 +1,5 @@
 
 
-import math
 
 
 
@@ -18,9 +17,9 @@ class Vehicle:
         self.max_speed = 300
         self.capacity = 100
 
-    def vroom(self):
-        for max_speed in range(300):
-            print("vroom")
+    # def vroom(self):
+    #     for max_speed in range(300):
+    #         print("vroom")
 
 class Bus(Vehicle):
     """A vehicle that is very large and carries
@@ -30,8 +29,23 @@ class Bus(Vehicle):
         fare: Cost of riding the bus
     """
 
-    # def __init__(self, age: float) -> None
+    def fare(self, age: float) -> None:
+
+        if age <= 17:
+            fare = 0
+
+        elif 18 >= age <= 60:
+            fare = 5
+
+        elif age > 60:
+            fare = 0
+
+        return fare
 
 
 this_car = Vehicle()
-this_car.vroom()
+# this_car.vroom()
+
+this_car = Bus()
+print(f"The fare of the bus ride is ${this_car.fare(12)}")
+
