2d4ba8204f1dac6d5fb73f6bb37cdcc16c9efd72
Tyler Bradley <tylergbradley05@gmail.com>
2021-11-15 09:46:12 -0800

    made infinite rectangle

--- dvd_screensaver.py
+++ dvd_screensaver.py
@@ -28,16 +28,27 @@ class Dvdimage:
         width: width of our rectangle in px
         height: height of our rectangle in px
         colour: 3-tuple of (r, g, b)
+        x-vel: velocity in px/sec
     """
     def __init__(self):
         self.x, self.y = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         self.width = 150
         self.height = 90
         self.colour = BLACK
+        self.x_vel = 5
+        self.y_vel = 3
+
 
     def rect(self) -> pygame.rect:
         """Returns a pygame.rect that represents the dvdimage"""
         return[self.x, self.y, self.width, self.height]
+
+    def update(self) -> None:
+        """Updates the Dvdimage with every tick"""
+        # Update the x coordinate
+        self.x += self.x_vel
+        # Update the y coordinate
+        self.y += self.y_vel
 
 
 def main() -> None:
@@ -59,6 +70,8 @@ def main() -> None:
                 done = True
 
         # ----------- CHANGE ENVIRONMENT
+        dvd_image.update()
+        print(f"x: {dvd_image.x}, y: {dvd_image.y}")
 
         # ----------- DRAW THE ENVIRONMENT
         screen.fill(BGCOLOUR)      # fill with bgcolor
