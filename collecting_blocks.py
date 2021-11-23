dea2ee940d36d8d4568d49c85d8a924f6a056675
Tyler Bradley <tylergbradley05@gmail.com>
2021-11-23 12:50:23 -0800

    made squares to collect

---
+++ collecting_blocks.py
@@ -0,0 +1,144 @@
+# Pygame Boilerplate
+# Author: Tyler
+
+
+import random
+import pygame
+import pygame.sprite
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0,   0,   0)
+RED = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE = (0,   0, 255)
+BGCOLOUR = (100, 100, 255)
+
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE  = "<<Collecting Blocks>>"
+
+class Player(pygame.sprite.Sprite):
+    """Describes a player object
+    A subclass of pygame.sprite.Sprite
+
+    Attributes:
+        image: Surface that is the visual
+            representation of or Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self) -> None:
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.image.load("./images/sprite_cranberry.png")
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
+
+class Block (pygame.sprite.Sprite):
+    """Describes a block object
+    A subclass of pygame.sprite.Sprite
+
+    Attributes:
+        image: Surface that is the visual
+            representation of or Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self, colour: tuple, width: int, height: int) -> None:
+        """
+        Arguments
+        :param colour: 3-tuple (r, g, b)
+        :param width: width in pixels
+        :param height: height in pixels
+        """
+        # Call the superclass constructor
+        super().__init__()
+
+
+        # Create the image of the block
+        self.image = pygame.Surface([width, height])
+        self.image.fill(RED)
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
+
+
+
+def main() -> None:
+    """Driver of the Python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(WINDOW_TITLE)
+
+    # Create some local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+    num_blocks = 100
+    score = 0
+
+    pygame.mouse.set_visible(False)
+
+    # Create groups to hold sprites
+    all_sprites = pygame.sprite.Group()
+    blocks_sprites = pygame.sprite.Group()
+
+    # Create all the block sprites and add to block_sprites
+    for i in range(num_blocks):
+
+        # Create a block (set its parameters)
+        block = Block(BLACK, 20, 15)
+
+    # Set a  random location for the block inside the screen
+        block.rect.x = random.randrange(SCREEN_WIDTH - block.rect.width)
+        block.rect.y = random.randrange(SCREEN_HEIGHT - block.rect.height)
+
+    # Add the block to the block_sprites Group
+    # Add the block to the all_sprites Group
+        blocks_sprites.add(block)
+        all_sprites.add(block)
+
+    # Create the Player block
+    player = Player()
+    # Add the player to all sprites group
+    all_sprites.add(player)
+
+    # ----------- MAIN LOOP
+    while not done:
+        # ----------- EVENT LISTENER
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ----------- CHANGE ENVIRONMENT
+        # Process player movement based on mouse pos
+        mouse_pos = pygame.mouse.get_pos()
+        player.rect.x, player.rect.y = mouse_pos
+
+        # Check all collisions between player and blocks
+        blocks_collided = pygame.sprite.spritecollide(player, blocks_sprites, True)
+
+        for block in blocks_collided:
+            score += 1
+            print(f"Score: {score}")
+
+        # ----------- DRAW THE ENVIRONMENT
+        screen.fill(BGCOLOUR)      # fill with bgcolor
+
+        # Draw all sprites
+        all_sprites.draw(screen)
+
+        # Update the screen
+        pygame.display.flip()
+
+        # ----------- CLOCK TICK
+        clock.tick(75)
+
+
+if __name__ == "__main__":
+    main()
